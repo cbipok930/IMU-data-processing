972f2ab02a26d6525d132141fa107eace03a133e
cbipok930 <a.shovkoplyas@g.nsu.ru>
2024-03-11 18:29:47 +0800

    record with regular freq

--- DumperDroneEKFgrav.py
+++ DumperDroneEKFgrav.py
@@ -4,6 +4,10 @@ import datetime
 import json
 
 from imu_bno import Imu
+import multiprocessing as mp
+
+import queue
+
 
 """
 
@@ -12,23 +16,71 @@ from imu_bno import Imu
 xi = [t, ax, ay, az, gx, gy, gz, magx, magy, magx, ekfx, ekfy, ekfz]
 
 """
+
+"""
+new way
+xi = [t (for debug), ax, ay, az, gx, gy, gz, magx, magy, magx, ekfx, ekfy, ekfz], regular freq=60 Hz
+"""
 DIRNAME = os.path.dirname(__file__)
 class Dumper():
     def __init__(self, sensor:Imu, filename = os.path.join(DIRNAME, datetime.datetime.now().strftime("%d.%m.%Y.%H_%M_%S.json")), *args, **kwargs) -> None:
 
         self.filename = filename
         self.sensor = sensor
+
+
+        self.__reader = None
+        self.__recorder = None
+        self.__shareData: mp.Queue = None
+        self.last_ts = None
+
+        # self.read = True
+
+    def read_process(self):
+        while True:
+            accX, accY, accZ = self.sensor.acceleration
+            gyroX, gyroY, gyroZ = self.sensor.gyro
+            magnX, magnY, magnZ = self.sensor.magnetic
+
+            dat_r = [[accX, accY, accZ], [gyroX, gyroY, gyroZ], [magnX, magnY, magnZ]]
+            # dat =  self.get_ekf_gravity(dat_r)
+            gX, gY, gZ = self.sensor.get_ekf_gravity(dat_r)
+            try:
+                self.__shareData.put([accX, accY, accZ, gyroX, gyroY, gyroZ,magnX, magnY, magnZ, gX, gY, gZ], block=True, timeout=0.01)
+            except queue.Full:
+                try:
+                    self.__shareData.get(block=False)
+                except queue.Empty:
+                    pass
+    
+    def record_process(self, duration=None, n_samp=None):
+        try:
+            with open(self.filename, 'x') as f:
+
+                self.sensor.init_ekf()
+                self.last_ts = time.time()
+
+                if duration is None:
+                    data = self.__read_and_write_nsamp(n_samp)
+                else:
+                    data = self.__read_and_write_duration(duration)
+                json.dump(data, f)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"No such directory: {os.path.dirname(self.filename)}")
     
     def __read_data_common(self, data):
-        t1 = time.time()
-        accX, accY, accZ = self.sensor.acceleration
-        gyroX, gyroY, gyroZ = self.sensor.gyro
-        magnX, magnY, magnZ = self.sensor.magnetic
-
-        gX, gY, gZ = self.sensor.get_ekf_gravity()
-
-        t2 = time.time()
-        data.append(((t1 + t2)/2, accX, accY, accZ, gyroX, gyroY, gyroZ, magnX, magnY, magnZ, gX, gY, gZ))
+        item = None
+        while self.__shareData.empty():
+            pass
+        item =  self.__shareData.get(block=True)
+        end = time.time()
+        #60Hz
+        while ((end - self.last_ts) < 0.0166666667):
+            end = time.time()
+            pass
+        item.insert(0, (end + self.last_ts)/2)
+        self.last_ts = end
+        data.append(item)
         return data
     
     def __read_and_write_duration(self, duration):
@@ -50,16 +102,6 @@ class Dumper():
             data = self.__read_data_common(data)
         return data
 
-    # def __dump(self, duration, n_samp):
-    #     try:
-    #         with open(self.filename, 'x') as f:
-    #             if duration is None:
-    #                 data = self.__read_and_write_nsamp(n_samp)
-    #             else:
-    #                 data = self.__read_and_write_duration(duration)
-    #             json.dump(data, f, indent=2)
-    #     except FileNotFoundError:
-    #         raise FileNotFoundError(f"No such directory: {os.path.dirname(self.filename)}")
     def record_data(self, filename = None, duration=None, blocking=False, n_samp=10):
         if filename is None:
             self.filename = os.path.join(DIRNAME, datetime.datetime.now().strftime("%d.%m.%Y.%H_%M_%S.json"))
@@ -69,18 +111,17 @@ class Dumper():
         #     self.__read_process = th.Thread(target=self.__dump, args=(duration, n_samp))
         #     self.__read_process.start()
         # else:
-        try:
-            with open(self.filename, 'x') as f:
+        self.__shareData = mp.Queue(3)
 
-                self.sensor.init_ekf()
+        self.__reader = mp.Process(target=self.read_process, daemon=True)
+        self.__recorder = mp.Process(target=self.record_process, args=(duration, n_samp), daemon=True)
 
-                if duration is None:
-                    data = self.__read_and_write_nsamp(n_samp)
-                else:
-                    data = self.__read_and_write_duration(duration)
-                json.dump(data, f)
-        except FileNotFoundError:
-            raise FileNotFoundError(f"No such directory: {os.path.dirname(self.filename)}")
+        self.__reader.start()
+        self.__recorder.start()
+
+        self.__recorder.join()
+        self.__reader.terminate()
+        self.__reader.kill()
     # def wait(self):
     #     self.__read_process.join()
     #     self.__read_process = None
